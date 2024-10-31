b33c316622d09c4cba6573cfa52ed8bf4967b597
Hi-ak <77273710+Hi-ak@users.noreply.github.com>
2024-10-31 21:45:01 +0900

    chore: 日付変更

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = ({}) => {
     <footer className={styles.footer}>
       <p className={styles.description}>
         &copy; Last updated on{" "}
-        <span className={styles.bold}>November 1st, 2024 </span>Hiroyuki Akiyama
+        <span className={styles.bold}>October 31st, 2024 </span>Hiroyuki Akiyama
       </p>
     </footer>
   );
