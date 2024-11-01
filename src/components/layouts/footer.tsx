6ec2f64e32cad6ac06ebd7ab1afbd6a54a0bcb86
Hi-ak <77273710+Hi-ak@users.noreply.github.com>
2024-11-01 14:22:00 +0900

    fix: footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -4,8 +4,8 @@ export const Footer = ({}) => {
   return (
     <footer className={styles.footer}>
       <p className={styles.description}>
-        &copy; Last updated on{" "}
-        <span className={styles.bold}>October, 2024 </span>Hiroyuki Akiyama
+        &copy; Last updated on <span className={styles.bold}>Oct. 2024 </span>
+        Hiroyuki Akiyama
       </p>
     </footer>
   );
